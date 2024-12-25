e3deb2da09d6aa22c41186640113785668852c1d
Himanshu Rai <himanshuraimau9@gmail.com>
2024-12-25 09:33:53 +0530

    Implement raw mode for terminal input handling and disable raw mode

--- texty.c
+++ texty.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+
+struct termios orig_termios; // a struct that stores the original terminal attributes
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // set the terminal attributes to the original terminal attributes
+}
+
+void enableRawMode(){
+    struct termios raw; // a struct that stores the terminal attributes
+    atexit(disableRawMode); // at exit, disable the raw mode
+
+    struct termios raw = orig_termios; // copy the original terminal attributes to raw
+
+    tcgetattr(STDIN_FILENO, &raw);  // get the terminal attributes
+    raw.c_lflag &= ~(ECHO | ICANON);   // turn off the echo and canonical mode
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // set the terminal attributes in the terminal
+}
+
 int main()
-{
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1);
+{  
+    enableRawMode(); // enable the raw mode
+    char c; 
+    while (read(STDIN_FILENO, &c, 1) == 1 && c!='q'); // read the input from the terminal and print it until the input is 'q'
     return 0;
 }
